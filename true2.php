244f89b056f259538fa77ec3da2861d89060a8af
billy <123406277+agrippajo@users.noreply.github.com>
2023-02-05 22:20:46 +0200

    Create true2.php

---
+++ true2.php
@@ -0,0 +1,8 @@
+ort com.google.common.io.BaseEncoding;
+import com.google.common.io.Resources;
+import com.google.protobuf.ByteString;
+
+import org.bitcoinj.core.AbstractBlockChain;
+import org.bitcoinj.core.Address;
+import org.bitcoinj.base.exceptions.AddressFormatException;
+import org.bitcoinj.base.Base5
